52508f528ce8d22b1c9d2708c16fd51ebf12d28b
Shivam Kumar <shivamkumar37je@gmail.com>
2025-10-25 12:01:58 +0530

    frontend

---
+++ Frontend/JS/script.js
@@ -0,0 +1,9 @@
+ document.getElementById("manu-box-of-nav").addEventListener("click",()=>{
+
+
+document.getElementById("inner-menu-box-of-nav").classList.toggle("ma1");
+document.getElementById("manu").classList.toggle("manu2");
+        
+
+
+});
